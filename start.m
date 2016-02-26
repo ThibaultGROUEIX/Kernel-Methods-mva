940beb528268042b1a7571d83dc9271110f15aac
Alexandre <alexhannebelle@gmail.com>
2016-02-26 14:49:20 +0100

    rien

--- start.m
+++ start.m
@@ -3,7 +3,17 @@ Xte=load('data/Xte.csv');
 Ytr=load('data/Ytr.mat');
 Ytr=Ytr.Ytr;
 
+%problem constants
+n=length(Xtr);
+
 %show(Xtr(2,:))
 
-%compute K
-K=zeros(length(Xtr));
+%compute f
+for num=1:10  %on regarde si l'image correspond au chiffre num-1
+    
+    alpha=(K+lambda*n*eye(n))\Ytr(:,2);
+    function [res]=f(im)
+    res=zeros(10,1);
+    gaussian_dist()
+    end
+end
